d1418dc12f2db211e163f8dd0edd736f82e72100
Emily Hannon <georgiannahannon@gmail.com>
2019-12-28 11:44:12 -0500

    driver period for clawbot. no auto period.

---
+++ VEXEDR2019/src/main.cpp
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Clawbot (Drivetrain) - Template                           */
+/*                                                                            */
+/*    Name:                                                                   */
+/*    Date                                                                    */
+/*    Class:                                                                  */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 2            
+// ClawMotor            motor         3               
+// ArmMotor             motor         4               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+}
